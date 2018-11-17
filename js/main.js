5aee5b86a3225332b2b62f79196004541ec1138b
Vladimir Mushegov <mushegov@mail.ru>
2018-11-17 20:30:20 +0600

    module2-task1 Пытаюсь настроить модули

--- js/main.js
+++ js/main.js
@@ -1,81 +1,87 @@
-'use strict';
+import {render} from 'utils';
 
-const templates = document.querySelectorAll(`template`);
-const main = document.querySelector(`section.main`);
-let currentScreenIndex = 0;
+console.log(render);
+render();
 
-const setScreenByIndex = (index) => {
-  if (index < 0) {
-    index = templates.length - 1;
-  }
 
-  if (index === templates.length) {
-    index = 0;
-  }
 
-  main.innerHTML = templates[index].innerHTML;
-  currentScreenIndex = index;
-};
+// console.log(utils);
 
-const prevScreen = () => setScreenByIndex(currentScreenIndex - 1);
-const nextScreen = () => setScreenByIndex(currentScreenIndex + 1);
+// const templates = document.querySelectorAll(`template`);
+// const main = document.querySelector(`section.main`);
+// let currentScreenIndex = 0;
+//
+// const setScreenByIndex = (index) => {
+//   if (index < 0) {
+//     index = templates.length - 1;
+//   }
+//
+//   if (index === templates.length) {
+//     index = 0;
+//   }
+//
+//   main.innerHTML = templates[index].innerHTML;
+//   currentScreenIndex = index;
+// };
+//
+// const prevScreen = () => setScreenByIndex(currentScreenIndex - 1);
+// const nextScreen = () => setScreenByIndex(currentScreenIndex + 1);
+//
+// const addKeyboardPressListener = () => {
+//   document.addEventListener(`keydown`, (event) => {
+//     const keyCode = event.key;
+//
+//     if (keyCode === `ArrowLeft`) {
+//       prevScreen();
+//     }
+//
+//     if (keyCode === `ArrowRight`) {
+//       nextScreen();
+//     }
+//
+//     return true;
+//   });
+// };
+//
+// const appendNavNode = () => {
+//   const app = document.querySelector(`main.app`);
+//   const navTemplate =
+//     `<style>
+//       .arrows__wrap {
+//         position: absolute;
+//         top: 135px;
+//         left: 50%;
+//         margin-left: -56px;
+//       }
+//       .arrows__btn {
+//         background: none;
+//         border: 2px solid black;
+//         padding: 5px 20px;
+//       }
+//     </style>
+//     <button class="arrows__btn"><-</button>
+//     <button class="arrows__btn">-></button>`;
+//   const navNode = utils(navTemplate, `arrows__wrap`);
+//
+//   app.append(navNode);
+// };
+//
+// const handleArrowNavigation = () => {
+//   const [prevArrow, nextArrow] = document.querySelectorAll(`.arrows__btn`);
+//
+//   prevArrow.addEventListener(`click`, () => prevScreen());
+//   nextArrow.addEventListener(`click`, () => nextScreen());
+// };
+//
+// const addArrowNavigation = () => {
+//   appendNavNode();
+//   handleArrowNavigation();
+// };
+//
+// setScreenByIndex(currentScreenIndex);
+//
+// addKeyboardPressListener();
+//
+// addArrowNavigation();
 
-const addKeyboardPressListener = () => {
-  document.addEventListener(`keydown`, (event) => {
-    const keyCode = event.key;
 
-    if (keyCode === `ArrowLeft`) {
-      prevScreen();
-    }
-
-    if (keyCode === `ArrowRight`) {
-      nextScreen();
-    }
-
-    return true;
-  });
-};
-
-const appendNavNode = () => {
-  const app = document.querySelector(`main.app`);
-  const navNode = document.createElement(`div`);
-  const navTemplate =
-    `<style>
-      .arrows__wrap {
-        position: absolute;
-        top: 135px;
-        left: 50%;
-        margin-left: -56px;
-      }
-      .arrows__btn {
-        background: none;
-        border: 2px solid black;
-        padding: 5px 20px;
-      }
-    </style>
-    <button class="arrows__btn"><-</button>
-    <button class="arrows__btn">-></button>`;
-
-  navNode.innerHTML = navTemplate;
-  navNode.classList.add(`arrows__wrap`);
-
-  app.append(navNode);
-};
-
-const handleArrowNavigation = () => {
-  const [prevArrow, nextArrow] = document.querySelectorAll(`.arrows__btn`);
-
-  prevArrow.addEventListener(`click`, () => prevScreen());
-  nextArrow.addEventListener(`click`, () => nextScreen());
-};
-
-const addArrowNavigation = () => {
-  appendNavNode();
-  handleArrowNavigation();
-};
-
-setScreenByIndex(currentScreenIndex);
-
-addKeyboardPressListener();
-
-addArrowNavigation();
---
+++ js/utils.js
@@ -0,0 +1,12 @@
+const render = () => {
+  // const element = document.createElement(`div`);
+  //
+  // element.innerHTML = template;
+  // element.classList.add(className);
+  //
+  // return element;
+
+  alert(`render`);
+};
+
+export render;
