7cbeef0ab8a646be4f721d0dcb782d0289ee85d6
Vladimir Mushegov <mushegov@kolesa.kz>
2018-11-27 20:22:20 +0600

    module3-task2 SR

--- js/main.js
+++ js/main.js
@@ -3,27 +3,3 @@ import {renderScreen} from './utils.js';
 
 renderScreen(screen);
 handleStartPlayButtonClick();
-
-
-const updateTimer = (time) => {
-  return time;
-};
-
-const gameTimer = (duration) => {
-  let timeLeft = duration;
-
-  const tick = () => {
-    timeLeft--;
-    updateTimer(timeLeft);
-
-    if (timeLeft === 0) {
-      clearInterval(timer);
-    }
-  };
-
-  const timer = setInterval(() => {
-    tick();
-  }, 1000);
-};
-
-gameTimer();
